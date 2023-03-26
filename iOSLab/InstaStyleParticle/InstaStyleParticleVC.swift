440c3e36d710e60ca8a005d5f883db23c473c04e
Danna <danna.wa@kakaobrain.com>
2023-03-26 22:58:41 +0900

    feat: make multiple emojis possible

--- iOSLab/InstaStyleParticle/InstaStyleParticleVC.swift
+++ iOSLab/InstaStyleParticle/InstaStyleParticleVC.swift
@@ -22,31 +22,37 @@ extension InstaStyleParticleVC {
         emitterLayer.emitterPosition = CGPoint(x: view.bounds.width/2, y: view.bounds.height - 100)
         emitterLayer.emitterSize = CGSize(width: 100, height: 100)
         emitterLayer.emitterShape = .point
-
-        let emitterCell = CAEmitterCell()
-        emitterCell.contents = {
-            let emoji = "🎉"
-            let font = UIFont.systemFont(ofSize: 20)
-            let size = emoji.size(withAttributes: [.font: font])
-            let renderer = UIGraphicsImageRenderer(size: size)
-            let image = renderer.image { context in
-                emoji.draw(at: .zero, withAttributes: [.font: font])
-            }
-            return image.cgImage
-        }()
-        emitterCell.birthRate = 20
-        emitterCell.lifetime = 1.0
-        emitterCell.velocity = 300
-        emitterCell.velocityRange = 50
-        emitterCell.emissionRange = .pi / 5
-        emitterCell.emissionLongitude = .pi / -2
-        emitterCell.alphaRange = 0.3
-        emitterCell.alphaSpeed = -0.5
-        emitterCell.scale = 0.15
-        emitterCell.scaleRange = 1
-        emitterCell.scaleSpeed = 0.5
         
-        emitterLayer.emitterCells = [emitterCell]
+        let emojiStrings = ["❤️", "💙", "💛", "💜", "🤎"]
+
+        var emitterCells: [CAEmitterCell] = []
+        for emoji in emojiStrings {
+            let emitterCell = CAEmitterCell()
+            emitterCell.contents = {
+                let font = UIFont.systemFont(ofSize: 20)
+                let size = emoji.size(withAttributes: [.font: font])
+                let renderer = UIGraphicsImageRenderer(size: size)
+                let image = renderer.image { context in
+                    emoji.draw(at: .zero, withAttributes: [.font: font])
+                }
+                return image.cgImage
+            }()
+            emitterCell.birthRate = 30 / Float(emojiStrings.count)
+            emitterCell.lifetime = 1.0
+            emitterCell.velocity = 300
+            emitterCell.velocityRange = 50
+            emitterCell.emissionRange = .pi / 5
+            emitterCell.emissionLongitude = .pi / -2
+            emitterCell.alphaRange = 0.3
+            emitterCell.alphaSpeed = -0.5
+            emitterCell.scale = 0.15
+            emitterCell.scaleRange = 1
+            emitterCell.scaleSpeed = 0.5
+            
+            emitterCells.append(emitterCell)
+        }
+        
+        emitterLayer.emitterCells = emitterCells
         view.layer.addSublayer(emitterLayer)
     }
 }
